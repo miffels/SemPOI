34f89aa89531196e5fb8c30516970f1a64417caa
Michael Jess <mijess@mail.uni-mannheim.de>
2013-10-24 13:58:11 +0200

    Adjusting POM, removing .gitkeep

--- server/src/main/java/de/unima/sempoi/server/SemPOI.java
+++ server/src/main/java/de/unima/sempoi/server/SemPOI.java
@@ -1,10 +1,13 @@
 package de.unima.sempoi.server;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import de.unima.sempoi.server.adapter.Freebase;
 
 /**
  * Servlet implementation class SemPOI
@@ -24,6 +27,7 @@ public class SemPOI extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().write("It works!");
+		new Freebase().readSightsOfCity("Berlin");
 	}
 
 	/**
